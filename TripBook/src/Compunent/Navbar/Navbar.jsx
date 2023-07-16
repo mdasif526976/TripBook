84a294a8d63510910595cccd9091ea365576b840
mdasif526976 <mdasif526976@gmail.com>
2023-07-16 08:12:18 +0600

    Navbar 1/2 complate

--- TripBook/src/Compunent/Navbar/Navbar.jsx
+++ TripBook/src/Compunent/Navbar/Navbar.jsx
@@ -1,9 +1,41 @@
 import React from "react";
-
+import { NavLink } from "react-router-dom";
+import image from "../../images/paper-airplane.png";
+import Aos from "aos";
+import "aos/dist/aos.css";
 const Navbar = () => {
+  Aos.init();
+  const menus = [
+    { name: "Home", to: "/" },
+    { name: "About", to: "/about" },
+    { name: "Destination", to: "/destination" },
+    { name: "Tour", to: "/tour" },
+    { name: "Blog", to: "/blog" },
+  ];
   return (
-    <div className="bg-[#ffffe8] px-[5%]">
-      <h1>from nabar</h1>
+    <div className="bg-[#ffffe8] px-[5%]" data-aos="fade-left">
+      {/* for web section */}
+      <div className="flex justify-between py-[45px] items-center ">
+        <div className="flex items-center">
+          <img className="w-10" src={image} alt="" />
+          <p className="font-semibold text-[24px]">TripBook</p>
+        </div>
+        <div className="flex gap-10">
+          {menus.map((menu, i) => (
+            <NavLink className="font-normal text-[16px]" key={i} to={menu.to}>
+              {menu?.name}
+            </NavLink>
+          ))}
+        </div>
+        <div className="flex gap-8">
+          <button className="py-4 px-7 bg-white text-[#faf336] rounded">
+            Login Now
+          </button>
+          <button className="py-4 px-7 text-white bg-[#faf336] rounded">
+            Sign up
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
